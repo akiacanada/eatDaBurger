b9a8b6a7c0796eef61d7a356c5e8ca3e1787a559
Akia Canada <akiacanada@yahoo.com>
2017-08-06 00:12:27 -0500

    second commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Plain Hamburger');
+INSERT INTO burgers(burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Crispy Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Tuna Burger');
